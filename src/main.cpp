b52d0673c120eb3d0d5108d6a98f5f49f851e64d
Dmitry Borisenko <49808844+DmitryBorisenko33@users.noreply.github.com>
2021-02-07 23:42:00 +0100

    node

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 String inMsg = "";
 uint32_t sleepingPeriod = 30 * 60 * 1000;  //первое число - минуты
-uint16_t attamptsNumber = 5;     //количество попыток повторных пересылок сообщений
+uint16_t attamptsNumber = 3;     //количество попыток повторных пересылок сообщений
 long totalErrors = 0;
 long fatalErrors = 0;
 
@@ -18,7 +18,7 @@ void before() {
     //NRF_NFCT->TASKS_DISABLE = 1; //останавливает таски, если они есть
     //NRF_UICR->NFCPINS = 0;  //отключает nfc и nfc пины становятся доступными для использования
     //NRF_NVMC->CONFIG = 0;   //
-    NRF_UART0->ENABLE = 0;  //отклчает uart, прежде чем использовать в коде не должно быть serial.print
+    //NRF_UART0->ENABLE = 0;  //отклчает uart, прежде чем использовать в коде не должно быть serial.print
 }
 
 void setup() {
@@ -35,41 +35,34 @@ void presentation() {
 }
 
 void loop() {
-    static int attempts = 0;
     float batteryVoltage = (float)hwCPUVoltage() / 1000.00;
 
-    sendMsgEchoAck(attempts, 0, 0, V_VOLTAGE, batteryVoltage, false);
-    wait(5);
-    SerialPrintln("==============================================");
-    sendMsgEchoAck(attempts, 0, 1, V_TEMP, random(1000, 1500), false);
-    wait(5);
-    SerialPrintln("==============================================");
-    sendMsgEchoAck(attempts, 0, 2, V_TEMP, random(100, 150), false);
-    wait(5);
-    SerialPrintln("==============================================");
-    sendMsgEchoAck(attempts, 0, 3, V_CUSTOM, fatalErrors, false);
-    wait(5);
-    SerialPrintln("==============================================");
-    sendMsgEchoAck(attempts, 0, 4, V_CUSTOM, totalErrors, true);  // у последнего сообщения в loop должно стоять true, у остальных false
-    SerialPrintln("==============================================");
-
-    //sendMsgFastAck(attempts, 0, V_VOLTAGE, batteryVoltage, false);
+    //sendMsgEchoAck(0, 0, V_VOLTAGE, batteryVoltage, false);
     //wait(5);
-    //SerialPrintln("===========================================");
-    //sendMsgFastAck(attempts, 1, V_TEMP, random(1000, 1500), false);
-    //wait(5);
-    //SerialPrintln("===========================================");
-    //sendMsgFastAck(attempts, 2, V_TEMP, random(100, 150), false);
-    //wait(5);
-    //SerialPrintln("===========================================");
-    //sendMsgFastAck(attempts, 3, V_CUSTOM, fatalErrors, false);
-    //wait(5);
-    //SerialPrintln("===========================================");
-    //sendMsgFastAck(attempts, 4, V_CUSTOM, totalErrors, true);  // у последнего сообщения в loop должно стоять true, у остальных false
     //SerialPrintln("==============================================");
+    //sendMsgEchoAck(0, 1, V_TEMP, random(1000, 1500), false);
+    //wait(5);
+    //SerialPrintln("==============================================");
+    //sendMsgEchoAck(0, 2, V_TEMP, random(100, 150), false);
+    //wait(5);
+    //SerialPrintln("==============================================");
+    //sendMsgEchoAck(0, 3, V_CUSTOM, fatalErrors, false);
+    //wait(5);
+    //SerialPrintln("==============================================");
+    //sendMsgEchoAck(0, 4, V_CUSTOM, totalErrors, true);  // у последнего сообщения в loop должно стоять true, у остальных false
+    //SerialPrintln("==============================================");
+
+  
+    sendMsgFastAck(0, V_VOLTAGE, batteryVoltage, false);
+    sendMsgFastAck(1, V_TEMP, random(1000, 1500), false);
+    sendMsgFastAck(2, V_TEMP, random(100, 150), false);
+    sendMsgFastAck(3, V_CUSTOM, fatalErrors, false);
+    sendMsgFastAck(4, V_CUSTOM, totalErrors, true);  // у последнего сообщения в loop должно стоять true, у остальных false
+    SerialPrintln("==============================================");
 }
 
-void sendMsgEchoAck(int &attempts, int nodeId, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep) {
+void sendMsgEchoAck(int nodeId, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep) {
+    static int attempts = 0;
     inMsg = "";
     MyMessage msg(ChildId, dataType);
     send(msg.setDestination(nodeId).setSensor(ChildId).set(value, 2), true);  //отправляем сообщение
@@ -98,16 +91,35 @@ void sendMsgEchoAck(int &attempts, int nodeId, int ChildId, const mysensors_data
     }
 }
 
-void sendMsgFastAck(int &attempts, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep) {
+//void sendMsgFastAck(int ChildId, const mysensors_data_t dataType, float value, bool goToSleep) {
+//    static int attempts = 0;
+//    MyMessage msg(ChildId, dataType);
+//    if (send(msg.set(value, 2), false)) {  //если отправилось
+//        attempts = 0;
+//        SerialPrintln("Msg " + String(ChildId) + " delivered, value = " + String(value));
+//        if (goToSleep) sleep(sleepingPeriod);
+//    } else {
+//        attempts++;
+//        totalErrors++;
+//        SerialPrintln("Msg " + String(ChildId) + " not delivered, attempt: " + String(attempts));
+//        wait(10);
+//        if (attempts >= attamptsNumber) {
+//            attempts = 0;
+//            fatalErrors++;
+//            SerialPrintln("Go to sleep, gate missing, try again after " + String(sleepingPeriod / 1000) + " sec");
+//            sleep(sleepingPeriod);
+//        }
+//    }
+//}
+
+void sendMsgFastAck(int ChildId, const mysensors_data_t dataType, float value, bool goToSleep) {
+    int attempts;
     MyMessage msg(ChildId, dataType);
-    if (send(msg.set(value, 2), false)) {  //если отправилось
-        attempts = 0;
-        SerialPrintln("Msg " + String(ChildId) + " delivered, value = " + String(value));
-        if (goToSleep) sleep(sleepingPeriod);
-    } else {
+    while (!send(msg.set(value, 2), false)) {  //если не отправилось
         attempts++;
         totalErrors++;
         SerialPrintln("Msg " + String(ChildId) + " not delivered, attempt: " + String(attempts));
+        _transportSM.failedUplinkTransmissions = 0;
         wait(10);
         if (attempts >= attamptsNumber) {
             attempts = 0;
@@ -116,6 +128,8 @@ void sendMsgFastAck(int &attempts, int ChildId, const mysensors_data_t dataType,
             sleep(sleepingPeriod);
         }
     }
+    SerialPrintln("Msg " + String(ChildId) + " delivered, value = " + String(value));
+    if (goToSleep) sleep(sleepingPeriod);
 }
 
 void receive(const MyMessage &message) {
--- src/main.h
+++ src/main.h
@@ -1,8 +1,8 @@
 //#define MY_DEBUG
 //#define MY_BAUD_RATE 115200
-//#define SERIAL_PRINT
-#define MY_PASSIVE_NODE
-#define MY_NODE_ID 100
+#define SERIAL_PRINT
+//#define MY_PASSIVE_NODE
+//#define MY_NODE_ID 100
 #define MY_RADIO_NRF5_ESB
 //#define MY_TRANSPORT_WAIT_READY_MS 10
 #include <MySensors.h>
@@ -13,8 +13,8 @@ extern uint16_t attamptsNumber;
 extern String inMsg;
 extern long totalErrors;
 
-extern void sendMsgEchoAck(int &attempts, int nodeId, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep);
-extern void sendMsgFastAck(int &attempts, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep);
+extern void sendMsgEchoAck(int nodeId, int ChildId, const mysensors_data_t dataType, float value, bool goToSleep);
+extern void sendMsgFastAck(int ChildId, const mysensors_data_t dataType, float value, bool goToSleep);
 extern void receive(const MyMessage &message);
 extern String parseToString(const MyMessage &message);
 extern void SerialPrintln(String text);
